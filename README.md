d0c5bbd2d0c06d1db613c6fb3820f09ac0c0658b
silesky <seth.silesky@gmail.com>
2016-10-19 14:23:25 -0500

    add select

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-* This project is using SystemJS for imports and Babel for es6.
+* This project is using SystemJS for imports and Babel for es6. 
+* I prefer jspm for smaller projects, as it's simpler to configure than webpack.
 * Development:
     * `npm install -g jspm`
     * `jspm install`
--- js/script.js
+++ js/script.js
@@ -1,20 +1,73 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+
+// grabs props from 
 class OrderForm extends React.Component {
-
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div>
-        
-      </div>
+    constructor(props) {
+        super(props);
+    }
+    // this should take a string prop that tells us if it's in-progress, cancelled, in progress, etc
+      render() {
+        return (
+          <div>
+          <div className="column">
+              <select value="cancelled">
+                <option value="in-progress">In Progress</option>
+                <option value="cancelled">Cancelled</option>
+                <option value="done">Done</option>
+            </select>
+            </div>
+          </div>
     );
-  }
+  } 
 }
-console.log('hello');
 
-ReactDOM.render(<OrderForm />, document.getElementById('root'));
+
+class FieldsContainer extends React.Component {
+      constructor(props) {
+        super(props);
+      }
+
+      getData: () => {
+        // fetch API goes here
+      }
+
+    // ajax request goes here
+    // state.map((el) => el.date etc
+    // state.map((el) => el.name etc
+    render() {
+    return (
+        <div className="fields">
+            <div className="column">
+                <h2>Name</h2>
+            </div>
+            <div className="column">
+                <h2>Technician</h2>
+            </div>
+            <div className="column">
+                <h2>Order Date</h2>
+            </div>
+            <div className="column">
+                <h2>Appt. Type</h2>
+            </div>
+            <div className="column">
+                <h2>Cell Number</h2>
+            </div>
+            <div className="column">
+                <h2>Email</h2>
+                  <h4>safassa</h4>
+                  <h4>safassa</h4>
+                  <h4>safassa</h4>
+                </div>
+             <div className="column">
+                <h2>Order Status</h2>
+                <OrderForm />
+            </div>
+        </div>
+    );
+    }
+}
+
+ReactDOM.render(<FieldsContainer />, document.getElementById('root'));
