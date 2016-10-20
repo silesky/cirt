d9b627104f898fece6a8c2031c2676af3abc97ca
silesky <seth.silesky@gmail.com>
2016-10-19 19:31:43 -0500

    add promises etc

--- js/script.js
+++ js/script.js
@@ -28,17 +28,50 @@ class OrderForm extends React.Component {
 class FieldsContainer extends React.Component {
       constructor(props) {
         super(props);
-      }
+        this.state = {
+          name: [],
+          technician: [],
+          orderDate: [],
+          apptType: [],
+          cellNumber: [],
+          email: [],
+          orderStatus: [],
+        };
+        this.getData = () => {
+          return fetch('data/data.json')
+            .then(response => response.json())
+        }
+        this.componentWillMount = () => {
+          this.getData().then(res => {
+            res.map(el => {
+              // store this in the state
+              this.setState( {name: [...this.state.name, el['Name']] } );
+              this.setState( {technician: [...this.state.technician, el['Technician']]} );
+              this.setState( {orderDate: [...this.state.technician, el['Order Date']]} );
+              this.setState( {apptType: [...this.state.apptType, el['Appt. Type']] });
+              this.setState( {cellNumber: [...this.state.cellNumber, el['Cell Number']] });
+              this.setState( {email: [...this.state.email, el['Email']] });
+              this.setState( {orderStatus: [...this.state.orderStatus, el['Order Status']] });
+            })
+        });
+        }
+        this.componentDidMount = () => console.log(this.state);
 
-      getData: () => {
-        // fetch API goes here
-      }
+     
+    }
+   
+
 
     // ajax request goes here
     // state.map((el) => el.date etc
     // state.map((el) => el.name etc
     render() {
+
+
+
+console.log(this.state);
     return (
+      
         <div className="fields">
             <div className="column">
                 <h2>Name</h2>
@@ -57,9 +90,8 @@ class FieldsContainer extends React.Component {
             </div>
             <div className="column">
                 <h2>Email</h2>
-                  <h4>safassa</h4>
-                  <h4>safassa</h4>
-                  <h4>safassa</h4>
+                <h4>email</h4>
+                
                 </div>
              <div className="column">
                 <h2>Order Status</h2>
