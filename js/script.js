fe94a29387fee0beaebce13d8a7aafc76521a72a
Seth Silesky <seth.silesky@gmail.com>
2016-10-23 20:54:39 -0500

    add padding, etc

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-import React from 'react'; import ReactDOM from 'react-dom';
+import React from 'react'; 
+import ReactDOM from 'react-dom';
 import { range } from 'lodash';
 import { searchObj } from '../util';
 
